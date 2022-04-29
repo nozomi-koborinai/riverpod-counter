582dd40124d009aad981a69413f479c7e644c9ed
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-04-29 15:04:13 +0900

    StateProvider作成

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,3 +1,4 @@
+import 'package:counter_riverpod/data/count_data.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final titleProvider = Provider<String>((ref) => 'Riverpod Demo App');
@@ -6,3 +7,6 @@ final messageProvider =
     Provider<String>((ref) => 'You have pushed the button this many times!!!:');
 
 final countProvider = StateProvider<int>((ref) => 0);
+
+final countDataProvider = StateProvider<CountData>(
+    (ref) => const CountData(count: 0, countUp: 0, countDown: 0));
